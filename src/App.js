6bff2496c6960ab248c0dc509bb7636106ff5f60
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-21 17:10:40 +0200

    create the FoodBox component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import foodsData from './foods.json'
+import FoodBox from './components/FoodBox'
 import { useState } from "react"
 import { Divider } from "antd"
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
   const [foods, setFoods] = useState(foodsData)
-  
 
   return (
     <div className="App">
+      
+      <FoodBox food={ {
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }} />
       
       <Divider>Food List</Divider>
       {foods.map(food => {
@@ -18,10 +26,6 @@ function App() {
           <img src={food.image} alt="food" width={"100px"} />
           </div>
       )})}
-
-
-      
-
     </div>
   );
 }
